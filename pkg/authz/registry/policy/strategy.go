f28f03db423147137e2530a28ac6ec5a0d56a97b
caryxychen <caryxychen@tencent.com>
2022-08-05 12:31:44 +0800

    bugfix

--- pkg/authz/registry/policy/strategy.go
+++ pkg/authz/registry/policy/strategy.go
@@ -117,8 +117,9 @@ func compactRules(rules []rbacv1.PolicyRule) []rbacv1.PolicyRule {
 		for _, rule := range rules {
 			apiGroups := rule.APIGroups
 			for j, _ := range rule.APIGroups {
-				log.Debugf("apiGroups: %s", apiGroups[j])
-				if apiGroups[j] == `""` {
+				log.Infof("apiGroups: %s", apiGroups[j])
+				if apiGroups[j] == "" || apiGroups[j] == "\"\"" || apiGroups[j] == "'\"\"'"{
+					log.Infof("matched: %s", apiGroups[j])
 					apiGroups[j] = ""
 				}
 			}
